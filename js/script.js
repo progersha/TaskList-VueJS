1d703a0d17032e6696fd8001e7e884c04a542977
progersha <ksyyustrifi@mail.ru>
2019-10-19 20:31:02 +0300

    Add tasks filters

--- js/script.js
+++ js/script.js
@@ -4,17 +4,36 @@ Vue.component('date-item', {
             today: new Date().toLocaleString('en', {weekday: 'long', month: 'short', day: 'numeric'}),
         }
     },
-    template: '<div>{{ today }}</div>',
+    template: '<div>{{ today }}</div>'
 });
 
-Vue.component('task-item',{
+Vue.component('filter-item', {
+    props: ['filter', 'active'],
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        filter_done() {
+            this.$emit('filter_done');
+        }
+    },
+    template: `
+        <li class="nav-item" @click="filter_done()">
+            <a :class="[{active: active}]" class="nav-link" href="#">
+                {{ filter.label }}
+            </a>
+        </li>
+    `
+});
+
+Vue.component('task-item', {
     props: ['data'],
     data () {
-        git remote set-url origin        return {
+        return {
             isDone: false,
-            text: true,
-            icon: 'fa fa-circle-thin co',
-            trash: 'fa fa-trash-o de'
+            icon: 'fa fa-circle-thin co'
         }
     },
     methods: {
@@ -22,21 +41,22 @@ Vue.component('task-item',{
             this.isDone = !this.isDone;
             this.icon = this.isDone ? 'fa fa-check-circle co' : 'fa fa-circle-thin co';
         },
-        task_done(){
-            this.$emit('task_done')
+        task_done() {
+            this.$emit('task_done');
         }
     },
     template: `
               <li class="item">
                     <i :class="[icon]" @click="toggleDone()"></i>
-                    <p :class="[{done: isDone, text}]"> {{ data.text }}</p>
-                    <i class="[trash]" @click="task_done()">️</i>
+                    <p :class="[{done: isDone}]" class="text"> {{ data.text }} </p>
+                    <i class="fa fa-trash-o de" @click="task_done()">️</i>
               </li>`
 });
 
 var vue = new Vue({
     el: "#app",
     data: {
+        currentFilterIndex: 0,
         new_task: {
             text: '',
         },
@@ -45,13 +65,38 @@ var vue = new Vue({
             { text: 'Cheese' },
             { text: 'Apple' }
         ],
+        filters: [
+            {
+                label: 'All',
+                status: "active",
+                id: 0
+            },
+            {
+                label: 'Active',
+                status: "",
+                id: 1
+            },
+            {
+                label: 'Closed',
+                status: "",
+                id: 2
+            },
+        ]
     },
     methods: {
         add_task(){
-
+            if(this.new_task.text != '') {
+                this.tasks.push({
+                    text: this.new_task.text,
+                });
+                this.new_task.text = '';
+            }
         },
-        delete_task(id){
-
+        delete_task(index){
+            this.tasks.splice(index,1);
+        },
+        set_filter(index) {
+            this.currentFilterIndex = index;
         }
     }
 });
